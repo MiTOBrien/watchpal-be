af296fd4c19d60ea50c7e927c7010185eee472ab
Michael OBrien <obrien.michael.t@gmail.com>
2025-11-25 08:09:13 -0500

    formats time so show time returns properly in serializer

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,4 +1,8 @@
 class ShowSerializer
   include JSONAPI::Serializer
-  attributes :id, :show_name, :air_day, :air_time, :channel_name, :channel_number, :available_on
+  attributes :id, :show_name, :air_day, :channel_name, :channel_number, :available_on
+
+  attribute :air_time do |object|
+    object.air_time&.strftime("%-I:%M %p") # e.g. "8:00 PM"
+  end
 end
